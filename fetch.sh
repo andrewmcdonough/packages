485da772ddc4670ff4161f5c6a9701d7ea20b97b
Simon Coffey <simon@tribesports.com>
2012-02-22 13:22:14 -0800

    Local copy of s3cmd

--- fetch.sh
+++ fetch.sh
@@ -3,7 +3,7 @@
 
 set -e
 mkdir -p debs.new
-s3cmd -c s3cfg-tribesports sync s3://tribesports-packages/pool/ debs.new/
+s3cmd/s3cmd -c s3cfg-tribesports sync s3://tribesports-packages/pool/ debs.new/
 cp debs/* debs.new
 rm -rf debs
 mv debs.new debs
